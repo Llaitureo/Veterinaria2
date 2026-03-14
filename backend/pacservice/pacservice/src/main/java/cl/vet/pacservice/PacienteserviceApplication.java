e76fa837a1dce86e5d3206d1989e40257e081318
Llaitureo <fe.llaitureo@duocuc.cl>
2026-03-13 22:28:18 -0300

    Estructura backend

---
+++ backend/pacservice/pacservice/src/main/java/cl/vet/pacservice/PacienteserviceApplication.java
@@ -0,0 +1,13 @@
+package cl.vet.pacservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PacienteserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PacienteserviceApplication.class, args);
+	}
+
+}
